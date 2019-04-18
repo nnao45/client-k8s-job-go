ccaf05d604d1c210576413ee1f18ff244d1e8793
nnao45 <n4sekai5y@gmail.com>
2019-04-18 15:06:31 +0900

    swtich job

--- client-k8s-job-go.go
+++ client-k8s-job-go.go
@@ -1,7 +1,73 @@
 package kjClient
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"path/filepath"
+
+	batchv1 "k8s.io/api/batch/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
 
 func Dummy() {
 	fmt.Println("hello")
+}
+
+func DummyClinet(manifests ...string) error {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		return err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return err
+	}
+
+	_ = clientset
+
+	for _, manifest := range manifests {
+		decode := scheme.Codecs.UniversalDeserializer().Decode
+		obj, _, err := decode([]byte(manifest), nil, nil)
+		if err != nil {
+			return err
+		}
+		switch o := obj.(type) {
+		case *batchv1.Job:
+			fmt.Println(o)
+		default:
+			return err
+		}
+	}
+
+	/*
+		jobsClient := clientset.BatchV1().Jobs("default")
+		job := &batchv1.Job{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "demo-job",
+				Namespace: "gitlab",
+			},
+			Spec: batchv1.JobSpec{
+				Template: apiv1.PodTemplateSpec{
+					Spec: apiv1.PodSpec{
+						Containers: []apiv1.Container{
+							{
+								Name:  "demo",
+								Image: "myimage",
+							},
+						},
+					},
+				},
+			},
+		}*/
+	return nil
 }
---
+++ example/main.go
@@ -0,0 +1,24 @@
+package main
+
+import kjClient "github.com/nnao45/client-k8s-job-go"
+
+const manifest = `
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: pi
+spec:
+  template:
+    spec:
+      containers:
+      - name: pi
+        image: perl
+        command: ["perl",  "-Mbignum=bpi", "-wle", "print bpi(2000)"]
+      restartPolicy: Never
+  backoffLimit: 4`
+
+func main() {
+	if err := kjClient.DummyClinet(manifest); err != nil {
+		panic(err)
+	}
+}
